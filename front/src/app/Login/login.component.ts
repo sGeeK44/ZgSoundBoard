f31f76f4afd4e0772e9f417a0c9d1cbb2034bde8
valentin <valentin.magrez@gmail.com>
2017-10-27 00:28:56 +0200

    Change lib to connect with google account

--- front/src/app/Login/login.component.ts
+++ front/src/app/Login/login.component.ts
@@ -1,65 +1,45 @@
 import { Component, OnInit, NgZone  } from '@angular/core';
-import { AuthService as GoogleAuthService, AppGlobals } from 'angular2-google-login';
-
+import { GoogleProfile } from './GoogleProfile'
+declare var gapi: any;
 
 @Component({
     selector: 'google-login',
     templateUrl: './login.component.html',
     styleUrls: ['./login.component.css'],
-    providers: [GoogleAuthService]
+    providers: []
   })
   export class LoginComponent implements OnInit {
-    imageURL: string;
-    email: string;
-    name: string;
-    token: string;
-  
-    constructor(private googleAuthService: GoogleAuthService, private zone: NgZone) { }
+    profile: GoogleProfile
+    fShowInfo = false;
+
+    constructor(private zone: NgZone) { }
   
     ngOnInit(): void {
-      AppGlobals.GOOGLE_CLIENT_ID = '862875253512-ets86drnkdbcc8v6ho7478basm0cvap1.apps.googleusercontent.com';
-      //Fake authent to counter issue in login plugin
-      this.googleAuthenticate()
-    }
-    /**
-     * Calling Google Authentication service
-     */
-    googleAuthenticate() {
-      this.googleAuthService.authenticateUser((result) => {
-        //Using Angular2 Zone dependency to manage the scope of variables
-        this.zone.run(() => {
-          this.getData();
+        gapi.signin2.render('my-signin2', {    
+            'onsuccess': param => this.onSignIn(param),
+            'scope': 'profile email',
+            'width': 240,
+            'height': 50,
+            'longtitle': false,
+            'theme': 'dark'
         });
-      });
     }
-  
-    /**
-     * Getting data from browser's local storage
-     */
-    getData() {
-      this.token = localStorage.getItem('token');
-      this.imageURL = localStorage.getItem('image');
-      this.name = localStorage.getItem('name');
-      this.email = localStorage.getItem('email');
-    }
-  
-    /**
-     * Logout user and calls function to clear the localstorage
-     */
-    logout() {
-      let scopeReference = this;
-      this.googleAuthService.userLogout(function () {
-        scopeReference.clearLocalStorage();
-      });
-    }
-  
-    /**
-     * Clearing Localstorage of browser
-     */
-    clearLocalStorage() {
-      localStorage.removeItem('token');
-      localStorage.removeItem('image');
-      localStorage.removeItem('name');
-      localStorage.removeItem('email');
+
+    onSignIn(googleUser) {
+        this.fShowInfo = true;
+        const basic_profile = googleUser.getBasicProfile();
+        this.zone.run(() => {
+            let id_token = googleUser.getAuthResponse().id_token;
+            this.profile = new GoogleProfile(basic_profile);
+            
+            //How use this token server side https://developers.google.com/identity/sign-in/web/backend-auth
+            this.profile.token_id = id_token
+        })   
+      };
+
+    signOut() {
+    var auth2 = gapi.auth2.getAuthInstance();
+    auth2.signOut();
+    this.fShowInfo = false;
     }
   }
