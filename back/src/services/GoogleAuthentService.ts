5869a43927ba0a5ed181d6ebc22ebfeb3bfdff1d
valentin <valentin.magrez@gmail.com>
2017-10-31 19:22:53 +0100

    Refactor and add get or create user on Authent

--- back/src/services/GoogleAuthentService.ts
+++ back/src/services/GoogleAuthentService.ts
@@ -1,25 +1,34 @@
 var request = require('request');
+import { UserRepository } from '../repository/UserRepository'
+import { IUser } from '../interfaces/IUser'
 
-export class GoogleAuthentService{
+class GoogleAuthentService{
 
-    static Authent(id_token){
+    private readonly GOOGLE_VERIFY_TOKEN_URL="https://www.googleapis.com/oauth2/v3/tokeninfo?"
+    private userRepository = new UserRepository;
 
+    public Authent(id_token){
+        const userRepo = new UserRepository();
         // Configure the request
         var options = {
-            url: 'https://www.googleapis.com/oauth2/v3/tokeninfo?id_token='+id_token,
-            method: 'get'
+            url: this.GOOGLE_VERIFY_TOKEN_URL+'id_token='+id_token,
         }
-
+        var user: IUser
         // Start the request
-        request(options, function (error, response, body) {
+        request.get(options, (error, response, body) => {
             if (!error && response.statusCode == 200) {
-                console.log(body)
+                const user_mail = JSON.parse(body).email
+                this.userRepository.getOrCreateByMail(user_mail, user)
+                console.log("user authent: "+user)
                 return true;
             }
             else{
-                console.log("une errreur");
+                console.log("Erreur lors de la requête d'authentification google");
                 return false;
             }
         })
     }
 }
+
+const googleAuthentService = new GoogleAuthentService
+export default googleAuthentService;
